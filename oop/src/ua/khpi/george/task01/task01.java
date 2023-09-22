799fbf175fcf98b70889c1dae77c4bb627f79d41
Heorhii Molchanov <heorhii_molchanov@epam.com>
2023-09-22 10:48:50 +0300

    repo: add task01 solution

--- oop/src/ua/khpi/george/task01/task01.java
+++ oop/src/ua/khpi/george/task01/task01.java
@@ -3,7 +3,11 @@ package ua.khpi.george.task01;
 public class task01 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		System.out.println("Begin...");
+		for (String param : args) {
+			System.out.println("Hello " + param);
+		}
+		System.out.println("Done.");
 	}
 
 }
